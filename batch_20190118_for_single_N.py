corrected sr, checked calibration in units, enlarged for eV conversion, updated all shooting days and rendered out every spectrum

batch_20190118_for_single_N.py
@@ -57,7 +57,7 @@ class batch_images_to_txt:
 
 
     def integration_unit_conversion(self):
-        from grating_function_20190118 import GratingCalculationOnPicture
+        from grating_function_20190121 import GratingCalculationOnPicture
         self.xlabel = "nm"
         self.ylabel = "integrated counts"
         my_picture = GratingCalculationOnPicture(self.file, self.xlabel, self.ylabel)
@@ -245,7 +245,7 @@ class Test_data_sets:
             None
         else:
             print("convert from eV into nm for file: ", self.file_name)
-            temp = test_file.eV_to_nm()
+            temp = test_file.nm_to_eV()
             header = np.zeros([1,2])
             new_calibration_file = np.concatenate((temp, header), axis=0)
             new_calibration_file.view('i8,i8').sort(order=['f0'], axis=0)

batch_20190121_for_single_N.py
@@ -245,7 +245,7 @@ class Test_data_sets:
             None
         else:
             print("convert from eV into nm for file: ", self.file_name)
-            temp = test_file.eV_to_nm()
+            temp = test_file.nm_to_eV()
             header = np.zeros([1,2])
             new_calibration_file = np.concatenate((temp, header), axis=0)
             new_calibration_file.view('i8,i8').sort(order=['f0'], axis=0)

batch_20190123_for_single_N.py
@@ -8,27 +8,6 @@ import matplotlib.pyplot as plt
 import os
 
 
-plt.close()
-path_picture = "rotated_20190123/"
-save_path = "results/"
-tif_files = []
-counter = 0
-
-
-for file in os.listdir(path_picture):
-    try:
-        if file.endswith(".tif"):
-            tif_files.append(str(file))
-            counter = counter + 1
-        else:
-            print("only other files found")
-    except Exception as e:
-        raise e
-        print("not files found here")
-
-
-print(len(tif_files), "number of files found")
-#np.savetxt("Files"+".txt",tif_files, delimiter='\t')
 
 
 class batch_images_to_txt:
@@ -142,15 +121,17 @@ class batch_images_to_txt:
         names = (['converted file:' + str(self.filename[20:]), "binning:" + str(self.result_binsize)])
         parameter_info = (
             ["integrated and resized binned spectra", "ROIsize spatial in px" + str(50) + ":" + str( 1600)])
-
+    #result binned_array [x,y]
         return np.vstack((parameter_info, names, header_names, self.result_binned_array))
 
     def save_to_txt(self):
         result = self.prepare_header()
-        print(result)
-        np.savetxt("20190123" +self.filename[20:] + ".txt", result, delimiter=' ',
-                   header='string', comments='', fmt = '%.s'
-                   )
+        #print(result)
+        np.savetxt( "20190123" + self.filename[20:-4] + "cal" + ".txt", result, delimiter=' ',
+                   header='string', comments='',
+                   fmt='%s')
+
+
 
 
     def resulting_binsize(self):
@@ -256,10 +237,11 @@ class Test_data_sets:
             None
         else:
             print("convert from eV into nm for file: ", self.file_name)
-            temp = test_file.eV_to_nm()
+            temp = test_file.nm_to_eV()
             header = np.zeros([1,2])
             new_calibration_file = np.concatenate((temp, header), axis=0)
             new_calibration_file.view('i8,i8').sort(order=['f0'], axis=0)
+            #save calibration file
             np.savetxt(self.file_name+ "_"+" nm"+ ".txt", new_calibration_file, fmt='%.3E', delimiter='\t')
             self.file_name = self.file_name+ "_"+" nm"+ ".txt"
 
@@ -302,6 +284,27 @@ def file_list_creater(liste_files_by_number, file_name_list):
 
 
 
+plt.close()
+path_picture = "data/rotated_20190123/"
+save_path = "results/"
+tif_files = []
+counter = 0
+
+
+for file in os.listdir(path_picture):
+    try:
+        if file.endswith(".tif"):
+            tif_files.append(str(file))
+            counter = counter + 1
+        else:
+            print("only other files found")
+    except Exception as e:
+        raise e
+        print("not files found here")
+
+
+print(len(tif_files), "number of files found")
+#np.savetxt("Files"+".txt",tif_files, delimiter='\t')
 
         
         
@@ -310,24 +313,12 @@ def file_list_creater(liste_files_by_number, file_name_list):
 file = path_picture + tif_files[10]
 
 wanted_binsize = 0.1
-
-
-
 Test = Test_data_sets(wanted_binsize, file)
-
 wanted_binsize = Test.minimum_binsize_possible()
-
 accuracy_decimal = Test.find_digit_and_round()
-
 print("accuracy", accuracy_decimal)
-
 x_max, x_min = Test.return_spectral_range()
-
-
 print('range of spectrum ', x_min, ' to ', x_max)
-
-
-
 
 cam_file = 'Andor_CCD_gain32.txt'
 grating_file = 'grating_efficiency_shimadzu.txt'
@@ -380,6 +371,7 @@ for x in range (0, how_many_files):
     temp2= batch1.convert_to_energy()
     #integrated_energy_list.append(my_picture_list[x] + description + str(boundary_high) + str(boundary_Low) + ' :  ')
     #integrated_energy_list.append(batch1.integrate_over_energy(boundary_Low, boundary_high))
+    #single harmonic number evaluation over the stack... resulting index is shotnumber
     integrated_energy_list.append(batch1.integrate_over_energy(boundary_Low, boundary_high))
     batch1.plot_my_result(temp2)
 

efficiency_function.py
@@ -12,152 +12,81 @@ class Prepare_efficiency_binning:
     def __init__(self, binsize, file, x_min, x_max, accuracy):
         
         self.result_binsize = binsize
-        
         self.filepath = file
-        
         self.file_name = str
-        
         self.calibration = np.array
-        
         self.new_file_name = str
-
         self.interpolated = np.array
-        
         self.ROI_on_interpolated = np.array
-
         self.min_x = x_min
-        
         self.max_x = x_max
-        
         self.accuracy = accuracy
-        
         #print(self.accuracy, 'prepare_efficiency')
-
         self.temp_list = np.array
-        
-
-
-
 
 
     def open_file(self):
-
         from linear_interpolation_Python3 import Load_text_into_2D_array
-
         my_calibration = Load_text_into_2D_array(self.filepath)
-        
         self.file_name = my_calibration.path_leaf()
-
-
-        
         self.calibration = my_calibration.loadarray()
-        
-        
         return self.calibration
         
 
     def interpolate_linear(self):
-
-       
         from linear_interpolation_Python3 import Linear_interpolation
-
         interpolation = Linear_interpolation(self.result_binsize, self.calibration, self.file_name, self.accuracy)
-        
         self.interpolated = interpolation.checkbinsize()
-
         print(self.interpolated, "self.interpolated after checkbinsize")
-
-
         return self.interpolated
 
-
-
     def return_accuracy(self):
-        
-
         print('accuracy in digit after 0', self.accuracy)
 
 
     def resize_range(self):
-
-
         print('resize_range', self.max_x, self.min_x)
-
         print(self.interpolated, "input_array at resize range")
-
         index_up = np.amax(np.where(self.interpolated[:,0] < self.max_x))
-        
         index_down = np.amin(np.where(self.interpolated[:,0] >= self.min_x))
-        
-
         print(self.interpolated[index_up], self.interpolated[index_down])
-        
         #print(index_up,' index up, corresponding to x_max')
         #print(index_down, " index down, corresponding to x_min'")
         #print(index_up-index_down, "number of entries after range")
-
-
         self.ROI_on_interpolated = self.interpolated[index_down:index_up]
         #print(self.ROI_on_interpolated)
-
-
-
         return self.ROI_on_interpolated
 
 
     def eV_to_nm(self):
-
-
         h = 4.136E-15
         order = 1E-9
         c = 3E8
-
         constant = h*c/order
-
         #print(constant, "transfer to nm.... constant")
-
         #print(self.calibration, "counts/photon in eV")
-
         self.temp_list = self.calibration
-
-
-
         in_nm = constant/(self.temp_list[:,0])
         self.temp_list[:,0] = in_nm
-
-
         self.calibration = self.temp_list
         self.calibration.view('i8,i8').sort(order=['f0'], axis=0)
-
         print(self.calibration, "counts/photon in nm")
-
         return self.calibration
 
 
 
+
+
+
 class calculate_efficiency_function:
-    
-    
-    
-
     def __init__(self, min_x, max_x, result_binsize, accuracy):
-        
-
-
         self.max_x = max_x
         self.min_x = min_x
         self.result_binsize = result_binsize
         self.accuracy = accuracy
-        
 
-        
-        
     def return_accuracy(self):
-        
         return self.accuracy
-        
-        
-        
 
     def get_efficiency_function(self):
         camera = Prepare_efficiency_binning(self.result_binsize,'Andor_CCD_gain32.txt' , self.min_x, self.max_x, self.accuracy)
@@ -165,49 +94,26 @@ class calculate_efficiency_function:
         camera.eV_to_nm()
 
         counts_per_photon_path = camera.interpolate_linear()
-
         a = camera.resize_range()
-        
-       # self.accuracy = camera.return_accuracy()
-
-
+        print(a)
         print('sub 001')
-
         grating = Prepare_efficiency_binning(self.result_binsize,'grating_efficiency_shimadzu.txt',self.min_x, self.max_x, self.accuracy)
         grating.open_file()
         grating_efficiency_path = grating.interpolate_linear()
-
         b = grating.resize_range()
-
-    
         print(b, "grating _function")
         print('sub 002')
-        
-        
-
         #np.savetxt("grating"+ "_"+"bin_size"+ repr(self.result_binsize)+".txt", b, fmt='%.3E', delimiter='\t')
-
         Al_filter = Prepare_efficiency_binning(self.result_binsize,'200nm_Al_filter.txt',self.min_x, self.max_x, self.accuracy)
         Al_filter.open_file()
         filter_efficiency_path = Al_filter.interpolate_linear()
-        
-        
-        
-
         c = Al_filter.resize_range()
-
-
         print('compare amount of bins')
         print(len(a), " Camera", len(b), "grating", len(c), "Al_filter")
-        
-        
         # bad fix for overhang offset has to be improved!!!
         if len(a) < len(b):
             print('mismatch by overhang')
-            
-
             b = b[: -1]
-            
             print(len(a), " Camera", len(b), "grating", len(c), "Al_filter")
             self.effiency_function= 3.5/(b[::,1]*c[::,1]*a[::,1])
             
@@ -224,29 +130,17 @@ class calculate_efficiency_function:
             
         else: 
             print('mismatch')
-            
-
 
         # get out the x_ axis for comparison.
-
         #print(self.effiency_function, len(self.effiency_function))
-
-
-
-
 
         plt.figure(1)
 
         plt.scatter( a[::,0],self.effiency_function, color = "b", marker="x")
         plt.xlabel("nm")
-        plt.ylabel("photons/counts")
+        plt.ylabel("photons/count")
 
         plt.show()
-
-
-
-
-
 
         return self.effiency_function
 

grating_function_20190121.py
@@ -27,55 +27,45 @@ class GratingCalculationOnPicture:
 
 
         def open_file(self):
-
             self.picture = plt.imread(self.filename)
             return self.picture
 
+
             
         def integrate_and_background(self):
-
             self.integrated = np.sum(self.x_backsubstracted[::, 50:1600], axis = 1)
             self.second_background()
             return self.integrated
 
-
         def second_background(self):
-
             print(len(self.integrated))
             for counter, value in enumerate(self.integrated):
-                self.integrated[counter] = value #+ 250000 + 100*counter
+                self.integrated[counter] = value + 50000 + 200*counter
             return self.integrated
 
 
         def background(self):
-
             back_mean=np.mean(self.picture[:, 1750:1800], axis = 1)
             for counter, value in enumerate(self.x_backsubstracted[::,0]):
-                
                 self.x_backsubstracted[::,counter] = self.picture[::,counter]- (back_mean[counter])
-
             self.integrate_and_background()
             print(np.amin(self.integrated[50:-50]), 'minimum .... after sum')
+
             return self.integrated
-
-
 
         
         def grating_function(self):
-
             N = len(self.integrated)
             i= 0
             while i <= N-1:
-                self.x_axis_in_nm[i] =1.24679344e-06 * i ** 2 - 1.65566701e-02 * i+ 5.22598053e+01
+                self.x_axis_in_nm[i] =1.22447518e-06*i**2 -1.73729829e-02*i+  5.82820234e+01
                 i = i+1
-
             self.array_of_spectrum()
             return self.result_array
 
 
 
         def array_of_spectrum(self):
-
             for x in range(0, len(self.x_axis_in_nm)):
                 self.result_array[x,0] = self.x_axis_in_nm[x]
                 self.result_array[x,1] = self.integrated[x]